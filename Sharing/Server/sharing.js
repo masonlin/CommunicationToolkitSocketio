0509a19c305e873d5a5029b6697791a829b59b81
Mason Lin (賣森林) <c.c.lin@g.ncu.edu.tw>
2016-08-17 00:28:09 +0800

    update

--- Sharing/Server/sharing.js
+++ Sharing/Server/sharing.js
@@ -24,41 +24,12 @@ io.on('connection', (socket) => {
   socket.on('s_file', (data) => {
 
     console.log('=====readFile=======');
-    console.log(data);
-    // var filename = path.basename(data.filename);
-    // var stream_out = ss_out.createStream();
+
     socket.emit('c_file', data);
     console.log('return to client ===');
 
-    //
-    //broadcast
-    //
-
-    //
-    // use dirrectly in/out stream //這裡再改成收到串流後，轉而將串流直接廣播出去給所有使用者
-    //
-
-
   });
 
-
-  // console.log('Client connected');
-  //
-  // socket.on('s_msg',function(msg){
-  //         console.log("JSON:" + msg);
-  //         socket.user = JSON.parse(msg).user;
-  //         socket.msg = JSON.parse(msg).msg;
-  //         console.log("new user:" + JSON.parse(msg).user + " logged.");
-  //
-  //         console.log('send msg to client s');
-  //
-  //         io.emit('c_msg',JSON.stringify({
-  //             user: socket.user,
-  //             msg: socket.msg
-  //         }));
-  //         console.log('send msg to client e');
-  //
-  // });
 
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
